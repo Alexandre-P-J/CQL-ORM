4552e64e87fe1656c0745ed99457cd43b3ee6862
pyro <EvilPyro@protonmail.com>
2022-12-06 00:10:00 +0100

    add example

---
+++ examples/main.py
@@ -0,0 +1,30 @@
+from uuid import UUID
+from cql_orm import UserDefinedType, Timestamp, Text, Int, Set, Decimal
+from cql_orm import ClusteringKeyColumn, PartitionKeyColumn, Column, Ordering, get_base_table
+
+BASE = get_base_table()
+
+
+class Employee(UserDefinedType):
+    employee_num = Int
+    name = Text
+    surname = Text
+    phones = Set(Text, frozen=True)
+
+
+class Address(UserDefinedType):
+    street = Text
+    city = Text
+    state = Text
+
+
+class Sensor(BASE):
+    id = PartitionKeyColumn(UUID)
+    capture = ClusteringKeyColumn(Timestamp, ordering=Ordering.Ascending)
+    installation = Column(Address)
+    contact = Column(Set(Employee))
+    temperature = Column(Decimal)
+    humidity = Column(Decimal)
+
+
+BASE.create_all("sensor_measurements")
